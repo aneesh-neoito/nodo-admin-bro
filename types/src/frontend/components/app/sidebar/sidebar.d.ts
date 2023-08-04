971527157e033ef38b893a8c59a16684a1cabf4a
Aneesh P Nair <aneesh.p@neoito.com>
2023-08-04 17:14:54 +0530

    first commit

---
+++ types/src/frontend/components/app/sidebar/sidebar.d.ts
@@ -0,0 +1,8 @@
+import React from 'react';
+declare type Props = {
+    isVisible: boolean;
+};
+declare const _default: React.ComponentType<Props & {
+    OriginalComponent?: React.FunctionComponent<Props> | React.ComponentClass<Props, any> | undefined;
+}>;
+export default _default;
---
+++ types/src/frontend/components/app/top-bar.d.ts
@@ -0,0 +1,9 @@
+import React from 'react';
+declare type Props = {
+    toggleSidebar: (any: any) => void;
+};
+export declare const TopBar: React.FC<Props>;
+declare const _default: React.ComponentType<Props & {
+    OriginalComponent?: React.FunctionComponent<Props> | React.ComponentClass<Props, any> | undefined;
+}>;
+export default _default;
